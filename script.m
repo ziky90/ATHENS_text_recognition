2f8b48b7a01dcd080e85ae50e9199f600a64630e
Jan Zikeš <ziky90@gmail.com>
2014-11-17 21:03:11 +0100

    added relative path

--- script.m
+++ script.m
@@ -1,7 +1,9 @@
 clear all;
 clean all;
 
-A = imread('/Users/zikesjan/Downloads/trainResized/14.Bmp', 'bmp')
+% here parametrize the path to the input image database
+A = imread('trainResized/14.Bmp', 'bmp')
+
 % TODO 1) iterate over whole folder -> perform turning to black and white ->
 % flatten and feed the classifier (do other feature extraction from image stuff, append features)
 
