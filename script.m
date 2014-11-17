e1ffe308f5b7cee3b4a726b8cc50b15dbfca0877
Jan Zikeš <ziky90@gmail.com>
2014-11-17 20:50:06 +0100

    initial commit

---
+++ script.m
@@ -0,0 +1,12 @@
+cleear all;
+clean all;
+
+A = imread('/Users/zikesjan/Downloads/trainResized/14.Bmp', 'bmp')
+% TODO 1) iterate over whole folder -> perform turning to black and white ->
+% flatten and feed the classifier (do other feature extraction from image stuff, append features)
+
+% save the learned model
+
+% load the classifier
+% o the sliding window + it's scalling => feature extraction + prediction
+% from  model
